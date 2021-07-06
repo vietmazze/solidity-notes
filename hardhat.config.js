282cdf2d3ec5310ad48e15f4e2911a5151054d8d
tangtang <gherson7827123@gmail.com>
2021-07-06 13:43:24 -0400

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+// Imports
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+
+    }
+  }
+};
