851f0216d17b5938ae918503b7fc758873f1c78f
tangtang <gherson7827123@gmail.com>
2021-12-28 11:08:12 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,31 @@
 // Imports
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.0",
-  paths: {
-    artifacts: './src/artifacts',
-  },
+ module.exports = {
+  defaultNetwork: "rinkeby",
   networks: {
     hardhat: {
-      chainId: 1337
-
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: {
+        mnemonic = process.env.MNEMONIC
+      },
+      chainId: 4,
     }
-  }
-};
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
